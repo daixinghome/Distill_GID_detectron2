351fd455dd2e7e75b79e8593187ad7bbdd391925
Xing Dai <xing.dai@shopee.com>
2023-01-31 06:20:42 +0000

    update experiment

--- RetinaNet_Res101/eval.sh
+++ RetinaNet_Res101/eval.sh
@@ -1,7 +1,7 @@
 ## {1}: config file path
 ## {2}: model weights file path
 
-export DETECTRON2_DATASETS=/data/disk1/datasets
+# export DETECTRON2_DATASETS=/data/disk1/datasets
 
 python3 train_net.py \
   --config-file ${1} \
